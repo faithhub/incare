54991684c78ee9631a0bacdc58feab9906e1363e
smart007 <babalolaferanmi8@gmail.com>
2020-11-22 21:09:34 +0100

    Revert "Message Model New Edit"
    This reverts commit 4059ea7a7665c246fd4ff9114d1f31d930486e80.

--- app/Models/Messages.php
+++ app/Models/Messages.php
@@ -11,8 +11,8 @@ class Messages extends Model
   protected $table = 'messages';
   protected $fillable = [
     'message',
-    'care_giver_id',
-    'employer_id',
+    'sender_id',
+    'receiver_id',
   ];
 
   public function create($data)
